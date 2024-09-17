3c3202a59a9bb1c5314fe397ad01162d87da01a9
jackykortrijk <jiaqi.zhao@ugent.be>
2024-09-17 16:56:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.title('基金ETF数据库')
 #添加筛选条件
 st.subheader('基金筛选条件：')
 with st.container():
-    col1, col2, col3, col4, col5 = st.column(5)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         agree = st.checkbox('显示指数实时折溢价情况')
         fee = st.checkbox('显示基金管理费率（%）')
