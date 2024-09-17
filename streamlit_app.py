f3ae4f5d9ea3092d834eaeb4667a2743b20c6e85
jackykortrijk <jiaqi.zhao@ugent.be>
2024-09-17 16:38:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 #应用标题
-st.set_option('deprecation.showPyplotGlobalUse', False)
+#st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(page_title="国元金工数据库展示", page_icon="random", layout="wide", initial_sidebar_state='auto')
 
 #标题文本设置
