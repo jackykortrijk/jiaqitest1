a370b36b472b03285cface41dd5cbd04f980bcce
jackykortrijk <jiaqi.zhao@ugent.be>
2024-09-17 16:29:55 +0200

    initial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
+import pandas as pd
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+#应用标题
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_page_config(page_title="国元金工数据库展示", page_icon="random", layout="wide", initial_sidebar_state='auto')
+
+#标题文本设置
+st.title('基金ETF数据库')
+
+#添加筛选条件
+st.subheader('基金筛选条件：')
